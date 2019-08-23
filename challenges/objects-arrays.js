5babbc620fe51ebc3e0b1a51aa4845248c64a868
Hector-bit <hectormartinez.2978@gmail.com>
2019-08-23 10:37:07 -0700

    all done except for one question

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -80,7 +80,7 @@ let universities = graduates.map(function(school){
   return school.university;
 });
 
-console.log(universities.sort);
+console.log(universities.sort());
 
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
@@ -123,6 +123,11 @@ The zoos want to display both the scientific name and the animal name in front o
 
 */
 const displayNames = [];
+
+zooAnimals.forEach(function(n){
+  return displayNames.push(`Name: ${n.animal_name}, Scientific: ${n.scientific_name}.`);
+});
+
 console.log(displayNames);
 
 /* Request 2: .map()
@@ -131,7 +136,10 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames = [];
+const lowCaseAnimalNames = zooAnimals.map(function(n){
+  return (n.animal_name.toLowerCase());
+});
+
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
@@ -139,17 +147,24 @@ console.log(lowCaseAnimalNames);
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals = [];
+const lowPopulationAnimals = zooAnimals.filter(function(n){
+  return n.population < 5;
+});
+
 console.log(lowPopulationAnimals);
+
 
 /* Request 4: .reduce() 
 
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = 0;
-console.log(populationTotal);
 
+const populationTotal = zooAnimals.reduce(function(total, n){
+  return total += n.population;
+}, 0);
+
+console.log(populationTotal);
 
 /*
 
